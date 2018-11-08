62f32b351dede3848237eb1e05b9cf03e013862c
zj <zj@mail.com>
2018-11-08 12:39:25 +0800

    发表文章完成

--- app.js
+++ app.js
@@ -5,13 +5,7 @@ const bodyParser = require('body-parser')
 const app = express()
 //导入session中间件
 const session = require('express-session')
-//注册session中间件
-//只要注册了session中间件 ,那么,今后只要能访问到req这个对象,必然能访问到req.session
-app.use(session({
-    secret: 'blog niubility',
-    resave: false,
-    saveUninitialized: false
-}))
+
 
 //设置默认的模板引擎
 app.set('view engine', 'ejs')
@@ -24,6 +18,14 @@ app.use('/node_modules', express.static('./node_modules'))
 
 // 注册body-parser中间件  注册以后才可以在req中使用body对象获取客户端post提交过来的数据
 app.use(bodyParser.urlencoded({ extended: false }))
+
+//注册session中间件
+//只要注册了session中间件 ,那么,今后只要能访问到req这个对象,必然能访问到req.session
+app.use(session({
+    secret: 'blog niubility',
+    resave: false,
+    saveUninitialized: false
+}))
 
 // //导入首页的路由模块
 // const indexRouter = require('./router/index.js')
